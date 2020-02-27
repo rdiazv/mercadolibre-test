62f08bce80630286f166c30c3d4ee71a0e1cdccf
Rodrigo Díaz <rdiazv89@gmail.com>
2020-02-27 20:34:36 -0300

    Test env

---
+++ __tests__/pages/index.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Home from '../../pages/index'
+import { mount } from 'enzyme'
+
+describe('Home', () => {
+  it('__pending__', () => {
+    mount(<Home />)
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testRegex: '.spec.tsx?$',
+  testEnvironment: 'jsdom',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/__tests__/'],
+  coverageReporters: ['html', 'text-summary', 'json'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json',
+    },
+  },
+}
