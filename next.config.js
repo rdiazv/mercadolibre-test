716a75a0a6c160785a9566e807f25824f82e1f2a
Rodrigo Díaz <rdiazv89@gmail.com>
2020-03-02 23:58:21 -0300

    PWA

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
+const withPWA = require('next-pwa')
 
-module.exports = withImages(
-  withCSS(
-    withSass({
-      sassLoaderOptions: {
-        includePaths: ['./src', './node_modules'],
-      },
-    })
+module.exports = withPWA(
+  withImages(
+    withCSS(
+      withSass({
+        sassLoaderOptions: {
+          includePaths: ['./src', './node_modules'],
+        },
+        pwa: {
+          dest: 'public',
+        },
+      })
+    )
   )
 )
